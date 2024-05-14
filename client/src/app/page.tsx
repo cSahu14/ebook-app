78084f0d0818c9447bbdf0649c13ce81129dddb5
cSahu14 <sahuchidananda1999@gmail.com>
2024-05-14 16:00:38 +0530

    add banner

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,10 @@
+import Banner from '@/components/Banner';
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>Welcome to ebook project</h1>;
+  return (
+    <>
+      <Banner />
+    </>
+  );
 }
