41329e50ab9093d17d7626b8885651282342196c
cSahu14 <sahuchidananda1999@gmail.com>
2024-05-14 17:05:36 +0530

    book card

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Banner from '@/app/(home)/components/Banner';
 import Image from 'next/image';
+import BookList from './components/BookList';
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(`${process.env.SERVER_URL}/books`);
+  if (!response.ok) {
+    throw new Error('An Error Occure While Fetch Book Data.');
+  }
+
+  const books = await response.json();
+  console.log(books);
   return (
     <>
       <Banner />
+      <BookList books={books} />
     </>
   );
 }
